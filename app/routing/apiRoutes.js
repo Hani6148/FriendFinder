ab8c89d649f2131ea537937368bec3ebe7350edf
deadpool <hanikhellafi@gmail.com>
2019-07-26 16:50:54 -0500

    just finished with the post route for /api/friends

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,5 +5,31 @@ app.get("/api/friends",function(req,res){
     res.json(friends)
 
 })
+app.post("/api/friends",function(req,res){
+     newFriend=req.body;
+     for (i=0;i<newFriend.scores.length;i++){
+       newFriend.scores[i]=parseInt(newFriend.scores[i]);
+ }
+     friends.push(newFriend);
+     console.log(friends);
+   bestMatch=100;
+   matchIndex=0;
+   
+     for (i=0;i<friends.length;i++){
+        matchScore=0;
+        for (j=0;j<friends[i].scores.length;j++){
+            matchScore+=Math.abs(newFriend.scores[j]-friends[i].scores[j])
+        }
+        if(matchScore<bestMatch){
+            matchIndex=i;
+            bestMatch=matchScore;
+
+        }
+     }
+
+     res.json(friends[matchIndex]);
+   
+})
+
 
 }
