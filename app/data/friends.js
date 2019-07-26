5c0893321a8b03626c0c2151b1312526fd445313
deadpool <hanikhellafi@gmail.com>
2019-07-26 17:02:28 -0500

    made some updates to the friends data

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,13 +5,13 @@ module.exports= [
         "scores":[
             5,
             1,
-            4,
+            2,
             4,
             5,
             1,
-            2,
-            5,
             4,
+            5,
+            3,
             1
           ]
       },
@@ -21,13 +21,13 @@ module.exports= [
         "scores":[
             5,
             1,
-            4,
+            5,
             4,
             5,
-            1,
+            3,
             2,
             5,
-            4,
+            1,
             1
           ]
       },
@@ -35,16 +35,16 @@ module.exports= [
         "name":"Susan",
         "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores":[
-            5,
+            1,
             1,
             4,
-            4,
+            3,
             5,
             1,
+            3,
+            5,
             2,
-            5,
-            4,
-            1
+            5
           ]
       },
       {
@@ -53,13 +53,13 @@ module.exports= [
         "scores":[
             5,
             1,
-            4,
-            4,
+            3,
             5,
-            1,
+            5,
+            4,
             2,
             5,
-            4,
+            3,            ,
             1
           ]
       },
@@ -71,10 +71,10 @@ module.exports= [
             1,
             4,
             4,
-            5,
+            4,
             1,
             2,
-            5,
+            1,
             4,
             1
           ]
